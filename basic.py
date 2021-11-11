e472ddf1d91e23a2ea2416d676d9bd62e18f13a3
meenamahto <93969718+meenamahto@users.noreply.github.com>
2021-11-11 15:07:13 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,127 @@
+# a=5
+# b=10
+# c=8 
+# print (a+b+c)
+
+# x="meena"
+# y="mahto"
+# print(x+y)
+
+# a="13/"
+# b="10/"
+# c="2021"
+# print(a+b+c)
+
+# num1="56"
+# num2=23
+# num3=int(num1)
+# print(num2+num3)
+
+# o="my name is meenamma"
+# p="so what i do"
+# print(o+p)
+# print(o,p)
+
+# a="meenamma"
+# b="2.5"
+# print(a+b)
+# print(type(b))
+# print(type(a))
+
+# a="12houses"
+# print(a)
+# print(2*a)
+
+# x="99"
+# y="meenamma"
+# print(x+y)
+
+
+# number = int(input("enter the number"))
+
+# print(number or 1)
+
+# a=2
+# b=5
+# c=a+b
+# a=c-a
+# b=c-b
+# print(a,b)
+
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a,b)
+
+# a=10
+# b=20
+# c=a+b
+# a=c-a
+# b=c-b
+# print(a,b)
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a,b)
+
+# a="5"
+# b=9
+# c=2.5
+# d=a+b+c
+# print(d)
+
+ # print(2587//8*2+(2**5)-0)
+
+# year="2016"
+# name="Navgurukul"
+# print(name+year ,"mein start hua tha.")
+
+# price_milk=int(input("enter 1l milk price"))
+# print("10l milk"+ price_milk*10+"rupees")
+
+# a="23/"
+# b="10/"
+# c="2021/"
+# print(a+b+c)
+
+# a="min"
+# b=2
+# print(str(b)+(a))
+
+# price_milk=int(input("enter number"))
+# print(price_milk*10,"rupay")
+
+# number=input("")
+# number1=int(number)
+# if number1%2==0:
+#     print("divisible")
+# else:
+#     print("not divisible")
+
+# a='25/'
+# b='10/'
+# c='2021'
+# print(a+b+c)
+
+
+# a="2"
+# print(complex(a))
+
+# a= 2+2j
+# b="2"
+# c=1
+# print(int(b)+a+c)
+# print(a+b+c)
+
+# a=2.3
+# print(complex(a))
+
+# a=1+1j
+# print(str(a))
+
+# a="meenamma"
+# print(len(a))
+
+# a=[1,2,3,4,5]
+# print(len(a))
+
---
+++ operators.py
@@ -0,0 +1,391 @@
+# x=25
+# y=10
+# print(x+y)
+
+# var1=5
+# var2=3
+# print(var1-var2)
+
+# a=2
+# b=3
+# print(a*b)
+
+# x=5
+# y=2
+# print(x/y)
+
+# num=5
+# num1=2
+# print(num%num1)
+
+# x=2
+# y=5
+# print(x**y)
+
+# x=15
+# y=2
+# print(x//y)
+
+
+# x=5
+# print(x)
+
+# x=5
+# y=10
+# x+=y
+# print(x)
+
+# x=10
+# y=5
+# x-=y
+# print(x)
+
+# x=5
+# y=10
+# x*=y
+# print(x)
+
+# x=50
+# y=10
+# x/=y
+# print(x)
+
+# x=25
+# y=10
+# x%=y
+# print(x)
+
+# x=5
+# y=2
+# x**=y
+# print(x)
+
+# x=5
+# y=2
+# x=x//y
+# print(x)
+
+
+# a=5
+# b=5
+# print(a==b)
+
+# a=6
+# b=9
+# print(a==b)
+
+# a=10
+# b=13
+# print(a!=b)
+
+# a=32
+# b=32
+# print(a!=b)
+
+
+# a=6
+# b=4
+# print(a>b)
+
+# x=7
+# y=9
+# print(x>y)
+
+# x=6
+# y=9
+# print(x<y)
+
+# x=13
+# y=10
+# print(x<y)
+
+# a=23
+# b=15
+# print(a>=b)
+
+# a=12
+# b=35
+# print(a>=b)
+
+# x=6
+# y=9
+# print(x<=y)
+
+# x=8
+# y=6
+# print(x<=y)
+
+# a=True
+# b=False
+# print(a and b and a)
+
+# a=4>5
+# a=899<887
+# print(a or a or 7<8)
+
+# a=19
+# b=34
+# c=56
+# print(not c>b)
+
+# var1=True
+# print(not var1)
+
+# a=True
+# b=False
+# print(a and b or a or not b)
+
+# a=5
+# a=a*"5"
+# print(type(a))
+# print(a)
+
+# var1="nav"
+# var2="gurukul"
+# print(type(var1+var2))
+
+# num1="7"
+# num2="number"
+# print(type(num1+num2))
+
+# num=6.8
+# num1=5
+# print(type(num+num1))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(1))
+
+# x=20
+# y=5
+# result=x+True+y-False
+# print(result)
+
+# str3='''hello'''""""""+"""world"""
+# print(str3)
+
+# num=6.8
+# print(num)
+# num=12
+# print(num)
+# print(num+num)
+# num=num+10
+# print(num)
+
+# a=5
+# b=7
+# print(a==b)
+
+# x=7
+# y=8
+# print(x!=y)
+
+# x=8
+# y=7
+# print(x>y)
+
+# a=10
+# b=9
+# print(b>a)
+
+# a=10
+# b=10
+# print(a>b)
+
+# x=10
+# y=100
+# print(x<y)
+
+# a=5
+# b=10
+# c=15
+# print(a>b and a<c)
+
+# a=11
+# b=22
+# c=23
+# print(a<b or b>c)
+
+# a=10
+# b=20
+# c=44
+# print(a==b and a<c)
+
+# a=10
+# b=40
+# c=50
+# print(not a==b)
+
+# list1=[10,20,30,]
+# list2=[20,30,40]
+# x=list1
+# print(x is list1)
+# print(x is  list2)
+
+# list1=[20,30,40]
+# list2=[20,30,40]
+# y=list2
+# print(y is not list2)
+# print(y is not list1)
+
+# x=40
+# y=20
+# a=[20,30,40,50]
+# print(x in a)
+# print(y not in a)
+
+# var123=2
+# var234=4
+# print(var123**var234)
+
+# nav12=12
+# nav23=2
+# print(nav12//nav23)
+
+# a=15
+# b=2
+# print(a%b)
+
+# a=30
+# b=40
+# print(a+b)
+
+# a="meena"
+# a="mahto"
+# print("meena"+a)
+
+# print(5>4 and 4>5)
+# print(4>5 or 5>5)
+# print(not False)
+
+# print(bool(False))
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+
+# a="12hours"
+# print(12+12,"hours")
+
+# x = 20
+# y = 5
+# result = x + True + y - False
+# print(result)
+
+# a=5
+# a=a+"5"
+# print(type(a))
+
+# var1="nav"
+# var2="gurukul"
+# print(type(var1+var2))
+
+# num1="7"
+# num2="number"
+# print(type(num1+num2))
+# print(num1+num2)
+
+# num=6.8
+# num1=5
+# print(type(num+num1))
+# print(num+num1)
+
+# a="meenamma"
+# print(a)
+
+# a="navgurukul"
+# b="2016"
+# print(a+","+b)
+# print(a+b)
+# print(a,b)
+
+# a="navgurukul"
+# b="13"
+# print(a+b)
+
+# a="12"
+# b="13.1"
+# print(a+b)
+
+# a=12
+# b=13.1
+# print(a+b)
+
+# x = 4
+# y = 5
+# print(x > y  is x>y)
+# print(x is y)
+
+# print(4==99)
+
+# print(10==10)
+
+# print("cat"!="dog")
+
+# print("mouse"!="mouse")
+
+# print(True and True)
+
+# print(False and True)
+
+# print(True or False)
+
+# print(False and False)
+
+# print(not False)
+
+# print(not True)
+
+# print(True and (4==4))
+
+# print("lion"=="car" or 99!=88)
+
+# x=4
+# y=5
+# print(x>y is x>y)
+
+# num1=4
+# num2=5
+# print(num1 is not num2)
+
+# a=True
+# b=False
+# print(a and b or a and b)
+# print(a or b is a or b )
+# print(not a is not a)
+
+# list=["apple","banana","cherry"]
+# print(list)
+
+# a=2.5
+# print(int(a))
+# print((2**a)/1+3%22*a-5)
+
+# print(3+2*(33//2)%5*2+2)
+# print(3.2//3//5+6%3+4//2 and 1)
+
+# print("a" or "b")
+
+# print(4 and 6)
+
+# print(4+6-4//2*3)
+
+# print(12//6+4-2%10)
+
+# print(6**2+4-6*10)
+
+# print(3**6+10-(617)-6+7-5*6)
+
+# print(3**5+6*8+(4//3))
+# 
+# name=["navgurukul,small,india"]
+# print( "navgurukul" in name)
+
+
+# print("t")
+# print("i")
+# print("n")
+# print("a")
+# a="meena"
+# print("m\ne\ne\nn\na")
